03f445bbbf57c4e1b6ba25e6916b035523fdba5d
Joseph P. Tata <joseph.p.tata@gmail.com>
2017-01-12 11:10:40 -0700

    Using module

--- example.tf
+++ example.tf
@@ -1,10 +1,6 @@
-provider "aws" {
+module "example" {
+  source = "github.com/josephptata/tf-module"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-}
-
-module "example" {
-  source = "github.com/josephptata/tf-module"
-
 }
