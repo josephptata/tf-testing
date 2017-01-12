fcf8114aab53b2d84a8b800eb5da9a4ed4cab300
Joseph P. Tata <joseph.p.tata@gmail.com>
2017-01-12 09:51:41 -0700

    Turn example into a module

--- exampe.tf
+++ exampe.tf
@@ -4,21 +4,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-b73d6cd7"
-  instance_type = "t2.micro"
+module "example" {
+  source = "github.com/josephptata/tf-module"
 
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
-}
-
-resource "aws_eip" "ip" {
-    instance = "${aws_instance.another.id}"
-}
-
-resource "aws_instance" "another" {
-  ami = "ami-b73d6cd7"
-  instance_type = "t2.micro"
-  depends_on = ["aws_instance.example"]
 }
